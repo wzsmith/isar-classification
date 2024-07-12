fix data leak (fr this time)

cnn_bilstm.py
@@ -6,6 +6,7 @@ from torch.optim import Adam
 from utils.seed import seed_everything
 from utils.utils import test, fit, predict
 from utils.model import CNN_BiLSTM
+from utils.constants import NUM_CLASSES, NUM_EPOCHS, LEARNING_RATE
 
 seed_everything(0)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -20,7 +21,7 @@ def main():
     args = parser.parse_args()
 
     # Initialize model
-    model = CNN_BiLSTM(num_classes=4)
+    model = CNN_BiLSTM(num_classes=NUM_CLASSES)
     model.to(device)
 
     # Ensure model path exists
@@ -38,10 +39,10 @@ def main():
     
     # Train model
     if args.train:
-        optimizer = Adam(model.parameters(), lr=1e-3)
+        optimizer = Adam(model.parameters(), lr=LEARNING_RATE)
         criterion = nn.CrossEntropyLoss()
         print('Training model!')
-        fit(model, optimizer, criterion, num_epochs=200, device=device)
+        fit(model, optimizer, criterion, num_epochs=NUM_EPOCHS, device=device)
         torch.save(model.state_dict(), model_path)
         print(f'Model saved at {model_path}')
     

dashboard-gr.py
@@ -2,12 +2,13 @@ import gradio as gr
 import torch
 from torchvision import transforms
 from utils.model import CNN_BiLSTM
+from utils.constants import NUM_CLASSES
 from PIL import Image
 import pandas as pd
 
 # Model Loading (outside of the Gradio interface)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model = CNN_BiLSTM(num_classes=4)
+model = CNN_BiLSTM(num_classes=NUM_CLASSES)
 model.load_state_dict(torch.load("weights/weights200.pth"))
 model.to(device)
 

utils/dataloader.py
@@ -1,13 +1,14 @@
 import torch
-from torch.utils.data import DataLoader, Subset
+from torch.utils.data import DataLoader
 from torchvision.io import read_image
 from torchvision import transforms
 from utils.isar_dataset import ISARDataset
+from utils.constants import DATA_PATH, LABEL_PATH
 
 TRANSFORM = transforms.CenterCrop((120, 120))
 
 def load_data(seq_length, batch_size):
-    dataset = ISARDataset('data/', 'labels.csv', seq_length=seq_length, transform=TRANSFORM)
+    dataset = ISARDataset(DATA_PATH, LABEL_PATH, seq_length=seq_length, transform=TRANSFORM)
 
     train_ratio = 0.8
     val_ratio = 0.10
@@ -19,8 +20,8 @@ def load_data(seq_length, batch_size):
         train_idx = start_idx + int(train_ratio * count)
         val_idx = start_idx + int((train_ratio + val_ratio) * count)
 
-        train_indices.extend(range(start_idx, train_idx))
-        val_indices.extend(range(train_idx, val_idx))
+        train_indices.extend(range(start_idx, train_idx - seq_length)) # Account for sequence going past split
+        val_indices.extend(range(train_idx, val_idx - seq_length))
         test_indices.extend(range(val_idx, start_idx + count))
         
         start_idx += count